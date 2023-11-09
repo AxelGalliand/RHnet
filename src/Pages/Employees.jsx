97958cec19f3e60dda3498c1cafb1eea92620d1c
Axel Galliand <galliandaxel88@gmail.com>
2023-11-09 16:52:23 +0100

    search bar adding

--- src/Pages/Employees.jsx
+++ src/Pages/Employees.jsx
@@ -6,121 +6,142 @@ import { useSelector } from 'react-redux';
 import { stateEmploye } from '../features/employe.slice';
 import styles from '../styles/EmployeeList.module.css';
 import { TableContent } from '../Component/TableContent';
+import chevUp from '../assets/chevronUp.svg';
+import chevDown from '../assets/chevronDown.svg';
 export function Employees () {
 
     const { data } = useSelector(stateEmploye);
     const [rows, setRows] = useState(data);
     console.log(rows)
-    // console.log(data);
-    /**
-     * 
-     */
-    const columns = [
+   
+    const [columns, setColumns] = useState([
       {
         dataField: 'firstName',
         text: 'First Name',
+        sortOrder: "ASC",
         type: "String",
       },
       {
         dataField: 'lastName',
         text: 'Last Name',
+        sortOrder: "ASC",
         type: "String",
       },
       {
         dataField: 'dateBirth',
         text: 'Date of birth',
+        sortOrder: "ASC",
         type: "Date",
       },
       {
         dataField: 'dateStart',
         text: 'Date of Start',
+        sortOrder: "ASC",
         type: "Date",
       },
       {
         dataField: 'street',
         text: 'Street',
+        sortOrder: "ASC",
         type: "String",
       },
       {
         dataField: 'city',
         text: 'City',
+        sortOrder: "ASC",
         type: "String",
       },
       {
         dataField: 'state',
         text: 'State',
+        sortOrder: "ASC",
         type: "String",
       },
       {
         dataField: 'zipCode',
         text: 'Zip Code',
+        sortOrder: "ASC",
         type: "Number",
       },
       {
         dataField: 'departement',
         text: 'Departement',
+        sortOrder: "ASC",
         type: "String",
       },
-    ];
-
-    const selectRow = () => {
-      console.log("row")
-    }
+    ]);
 
     const sortRow = (column) => {
       console.log(column);
-      
+      const columnsUpdated = [...columns];
+      const rowsSorted = [...rows];
+      // on trie par ordre croissant
+      if (column.type === "Date" || column.type === "Number") {
+        // object.mapropriete
+        // object["mapropriete"]
+        rowsSorted.sort((a,b) => {
+          return new Date(a[column.dataField]) - new Date(b[column.dataField])
+        })
+
+      } 
+      else {
+        rowsSorted.sort((a,b) => a[column.dataField].toLowerCase().localeCompare(b[column.dataField].toLowerCase()))
+      }
+
+      if(column.sortOrder === "ASC") {
+        column.sortOrder = "DSC"
+      }else {
+        // on inverse l'order des rows
+        rowsSorted.reverse()
+        column.sortOrder = "ASC"
+      }
+
+      // on met à jour la column
+      // on récupère l'index de la column à mettre à jour
+      const indexColumn = columnsUpdated.findIndex((col) => col.dataField === column.dataField)
+      // on met à la column dans le tableau
+      columnsUpdated[indexColumn] = column;
+      // on met à jour les states
+      setColumns(columnsUpdated)
+      setRows(rowsSorted)
     }
 
+    const SearchBar = () => {
+      const rowsSerchSorted = [...rows];
+
+    addEventListener('keyup', function (e) {
+      const input = e.target.value.toLowerCase();
+      let Filter = [];
+      for(let i = 0; i < rowsSerchSorted.length; i++ ) {
+        if (rowsSerchSorted[i].firstName.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].lastName.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].dateBirth.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].dateStart.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].street.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].city.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].state.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].zipCode.toLowerCase().includes(input) ||
+            rowsSerchSorted[i].departement.toLowerCase().includes(input)) Filter.push(rowsSerchSorted[i]);
+            console.log(Filter);
+      }})}
 
      
 
     return (
         <main>
           <div className={styles["TableDiv"]}>
-            {/* <Table data={employees} columns={columns} select={selectRow}/>  */}
+          <input type='text' className={styles["TableSerch"]} placeholder='Recherche' onChange={SearchBar}></input>
             <Table striped bordered hover>
               <thead>
                 <tr>
-                {/* <th>City</th>
-                <th>Zip code</th>
-                <th>Departement</th> */}
-                  {columns.map((column) => <th key={column.dataField} onClick={() => sortRow(column)}>{column.text}</th>)}
+                  {columns.map((column) => <th key={column.dataField} onClick={() => sortRow(column)}>
+                    {column.text} {column.sortOrder === "ASC" ? <img src={chevDown}/> : <img src={chevUp}/>}
+                  </th>)}
                 </tr>
               </thead>
                 <TableContent columns={columns} data={rows}/>
             </Table>   
-
           </div>
         </main>
     )
 }
-
-
-// { firstName: "Fabien",
-//             lastName: "TORREZ",
-//             dateBirth: "1982-10-02",
-//             dateStart: "2000-05-14",
-//             street: "5 rue du domaine",
-//             city: "Los santos",
-//             state: "NY",
-//             zipCode: "52000",
-//             departement: "sale"},
-//            {firstName: "Dona",
-//             lastName: "De MONTAZAC",
-//             dateBirth: "1989-06-08",
-//             dateStart: "2002-08-12",
-//             street: "8 rue du domaine",
-//             city: "Los santos",
-//             state: "NY",
-//             zipCode: "52000",
-//             departement: "Marketing"},
-//            {firstName: "Jonny",
-//             lastName: "MONNEY",
-//             dateBirth: "1975-02-17",
-//             dateStart: "2010-01-06",
-//             street: "25 route okay",
-//             city: "Hyerstate",
-//             state: "AK",
-//             zipCode: "07580",
-//             departement: "Legal"}
