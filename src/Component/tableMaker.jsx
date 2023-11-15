e81723b2567771e580ec27fea41c48c40715e902
Axel Galliand <galliandaxel88@gmail.com>
2023-11-15 19:16:37 +0100

    innit json data

---
+++ src/Component/tableMaker.jsx
@@ -0,0 +1,163 @@
+// write composant display all employees in tanle
+// 1. import React
+import React, { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { useSelector } from 'react-redux';
+import { stateEmploye } from '../features/employe.slice';
+import styles from '../styles/tableMaker.module.css';
+import { TableContent } from '../Component/TableContent';
+import chevUp from '../assets/chevronUp.svg';
+import chevDown from '../assets/chevronDown.svg';
+export function TableMaker () {
+
+    async function fetchData() {
+        return fetch("../data/employee.json")
+        .then(function(result) {
+          if(result.ok) {
+            return result.json();
+          }
+        })
+      }
+      console.log(fetchData());
+
+
+    const { data } = useSelector(stateEmploye);
+    const [rows, setRows] = useState(data);
+   
+    const [columns, setColumns] = useState([
+      {
+        dataField: 'firstName',
+        text: 'First Name',
+        sortOrder: "ASC",
+        type: "String",
+      },
+      {
+        dataField: 'lastName',
+        text: 'Last Name',
+        sortOrder: "ASC",
+        type: "String",
+      },
+      {
+        dataField: 'dateBirth',
+        text: 'Date of birth',
+        sortOrder: "ASC",
+        type: "Date",
+      },
+      {
+        dataField: 'dateStart',
+        text: 'Date of Start',
+        sortOrder: "ASC",
+        type: "Date",
+      },
+      {
+        dataField: 'street',
+        text: 'Street',
+        sortOrder: "ASC",
+        type: "String",
+      },
+      {
+        dataField: 'city',
+        text: 'City',
+        sortOrder: "ASC",
+        type: "String",
+      },
+      {
+        dataField: 'state',
+        text: 'State',
+        sortOrder: "ASC",
+        type: "String",
+      },
+      {
+        dataField: 'zipCode',
+        text: 'Zip Code',
+        sortOrder: "ASC",
+        type: "Number",
+      },
+      {
+        dataField: 'departement',
+        text: 'Departement',
+        sortOrder: "ASC",
+        type: "String",
+      },
+    ]);
+
+    const sortRow = (column) => {
+      console.log(column);
+      const columnsUpdated = [...columns];
+      const rowsSorted = [...rows];
+      // on trie par ordre croissant
+      if (column.type === "Date" || column.type === "Number") {
+        // object.mapropriete
+        // object["mapropriete"]
+        rowsSorted.sort((a,b) => {
+          return new Date(a[column.dataField]) - new Date(b[column.dataField])
+        })
+
+      } 
+      else {
+        rowsSorted.sort((a,b) => a[column.dataField].toLowerCase().localeCompare(b[column.dataField].toLowerCase()))
+      }
+
+      if(column.sortOrder === "ASC") {
+        column.sortOrder = "DSC"
+      }else {
+        // on inverse l'order des rows
+        rowsSorted.reverse()
+        column.sortOrder = "ASC"
+      }
+
+      // on met à jour la column
+      // on récupère l'index de la column à mettre à jour
+      const indexColumn = columnsUpdated.findIndex((col) => col.dataField === column.dataField)
+      // on met à la column dans le tableau
+      columnsUpdated[indexColumn] = column;
+      // on met à jour les states
+      setColumns(columnsUpdated)
+      setRows(rowsSorted)
+    }
+
+    const SearchBar = () => {
+      
+      addEventListener('change', function (e) {
+        const input = e.target.value.toLowerCase();
+        let filter = [];
+        console.log(input, data)
+        filter = data.filter((row) => {
+          return (
+            row.firstName.toLowerCase().includes(input) ||
+            row.lastName.toLowerCase().includes(input) ||
+            row.dateBirth.toLowerCase().includes(input) ||
+            row.dateStart.toLowerCase().includes(input) ||
+            row.street.toLowerCase().includes(input) ||
+            row.city.toLowerCase().includes(input) ||
+            row.state.toLowerCase().includes(input) ||
+            row.zipCode.toLowerCase().includes(input) ||
+            row.departement.toLowerCase().includes(input)
+          )
+        })
+        console.log(filter)
+        setRows(filter)
+
+      })
+
+    }
+
+
+    return (
+        
+          <div className={styles["TableDiv"]}>
+          <input type='text' className={styles["TableSerch"]} placeholder='Recherche' onChange={SearchBar}></input>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  {columns.map((column) => <th className={styles["columnTitle"]} key={column.dataField} onClick={() => sortRow(column)}>
+                    {column.text} {column.sortOrder === "ASC" ? <img src={chevDown}/> : <img src={chevUp}/>}
+                  </th>)}
+                </tr>
+              </thead>
+                <TableContent columns={columns} data={rows}/>
+            </Table>   
+          </div>
+        
+    )
+}
