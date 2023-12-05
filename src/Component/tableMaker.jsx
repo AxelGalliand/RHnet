c88d74489dee5559f15c9a5aa26bf356b16dfd58
Axel Galliand <galliandaxel88@gmail.com>
2023-12-05 16:43:48 +0100

    working pagination

--- src/Component/tableMaker.jsx
+++ src/Component/tableMaker.jsx
@@ -8,17 +8,20 @@ import styles from '../styles/tableMaker.module.css';
 import TableContent from '../Component/TableContent';
 import chevUp from '../assets/chevronUp.svg';
 import chevDown from '../assets/chevronDown.svg';
+import chevRight from '../assets/chevronRight.svg';
+import chevLeft from '../assets/chevronLeft.svg';
 import PropTypes from 'prop-types';
+import { current } from '@reduxjs/toolkit';
 
 const TableMaker = (props) => {
 
     const data = [...props.rows]
     const [rows, setRows] = useState([...props.rows]);
     const [columns, setColumns] = useState(props.columns);
-    const [slice, setSlice] = useState(10)
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [search, setSearch] = useState("")
-    const [totalPage, setTotalPage] = useState(Math.ceil(data.length / slice))
+    const [slice, setSlice] = useState(10);
+    const [search, setSearch] = useState("");
+    const [totalPage, setTotalPage] = useState(Math.ceil(data.length / slice));
+    const [currentPage, setCurrentPage] = useState(1);
     const sortRow = (column) => {
       console.log(column);
       const columnsUpdated = [...columns];
@@ -84,23 +87,74 @@ const TableMaker = (props) => {
       const number = +e.target.value
       setSlice(number)
       setTotalPage(Math.ceil(data.length / number))
+      setCurrentPage (1)
+      setRows(data.slice(0, number))
+    }
+  
+    const range = (from, to, step = 1) => {
+      let i = from;
+      const range = [];
+    
+      while (i <= to) {
+        range.push(i);
+        i += step;
+      }
+    
+      return range;
     }
 
+    const changePage = (numberPage) => {
+      setCurrentPage(numberPage)
+      setRows(data.slice((numberPage - 1) * slice, numberPage * slice))
+    }
 
+    const firstPage = () => {
+      setCurrentPage(1)
+    }
+
+    const lastPage = () => {
+      setCurrentPage(totalPage)
+    }
+
+    const previousPage = () => {
+      setCurrentPage(currentPage - 1)
+      setRows(data.slice((currentPage - 2) * slice, (currentPage - 1) * slice))
+    }
+
+    const nextPage = () => {
+      setCurrentPage(currentPage + 1)
+      setRows(data.slice(currentPage * slice, (currentPage + 1) * slice))
+    }
+    
     return (
         
         <div className={styles["TableDiv"]}>
           <input type='text' className={styles["TableSerch"]} placeholder='Recherche' onChange={SearchBar}></input>
           <div className={styles["sizeButton"]}>
-          <select className={styles["sizeSelect"]} onChange={SliceBy}>
-            <option value={10}>10</option>
-            <option value={25}>25</option>
-            <option value={50}>50</option>
-            <option value={100}>100</option>
-          </select>
-          <div className="pagination">
+            <select className={styles["sizeSelect"]} onChange={SliceBy}>
+              <option value={10}>10</option>
+              <option value={25}>25</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+            </select>
+          </div>
+          <div className={styles["pagination"]}>
               nombre de page {totalPage}
-            </div> 
+          </div>
+          <div className={styles["paginate"]}>
+
+            <button onClick={() => firstPage()}>1</button>
+            {currentPage === 1 ? null : <button onClick={() => previousPage()}><img src={chevLeft}/></button>}
+
+            {range(2, totalPage-1).map((numberPage) => {
+              return (
+                <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}>
+                  {numberPage}
+                </button>
+              )
+            })}
+            {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><img src={chevRight}/></button>}
+            {totalPage === 1 ? null : <button onClick={() => lastPage()}>{totalPage}</button>}
           </div>
             <Table striped bordered hover>
               <thead>
@@ -113,6 +167,22 @@ const TableMaker = (props) => {
                 <TableContent columns={columns} data={rows}/>
             </Table>  
         
+          <div className={styles["paginate"]}>
+
+            <button onClick={() => firstPage()}>1</button>
+            {currentPage === 1 ? null : <button onClick={() => previousPage()}><img src={chevLeft}/></button>}
+
+            {range(2, totalPage-1).map((numberPage) => {
+              return (
+                <button key={numberPage} onClick={() => changePage(numberPage)} className={currentPage === numberPage ? 'active' : null}>
+                  {numberPage}
+                </button>
+              )
+            })}
+            {currentPage === totalPage ? null : <button onClick={() => nextPage()} ><img src={chevRight}/></button>}
+            {totalPage === 1 ? null : <button onClick={() => lastPage()}>{totalPage}</button>}
+          </div>
+
         </div>
     )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import employeSlice from "../features/employe.slice";
-import paginationSlice from "../features/pagination.slice";
 
 export default configureStore({
     reducer: {
         employe: employeSlice,
-        pagination: paginationSlice
     }
 })
